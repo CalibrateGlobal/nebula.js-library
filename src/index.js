12998eb924f88bbc814b3c037b715b3ccab2896d
rellisbrown <rellisbrown@calibrateconsulting.com>
2022-08-03 13:01:10 +0100

    JDTP-38 - Deploy app as QMC Extension using Github Action

--- src/index.js
+++ src/index.js
@@ -3,17 +3,18 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import { GlobalStyle } from './globalStyles';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 const getBaseName = () => {
   if (process.env.NODE_ENV === 'production') {
     return '/extensions/nebula-lib/index.html';
   }
   return '';
 };
-console.log(process.env.NODE_ENV);
+
 root.render(
   <BrowserRouter basename={getBaseName()}>
     <GlobalStyle />
